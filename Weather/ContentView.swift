bad042bd0e63399f5e83a1781d6938ba06ad4c2b
Pave1iOS <PGribachev94@yandex.ru>
2024-03-18 15:41:21 +0300

    add function calculate

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -9,16 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        GeometryReader { geometry in
+            let width = geometry.size.width // x
+            let height = geometry.size.height // y
+            let size = min(width, height)
+//            let middle = size / 2
+            
+            Path { path in
+                // puff (green)
+                path.move(to: CGPoint(x: calculate(side: width, toPoint: 65), y: calculate(side: height, toPoint: 35)))
+                
+                path.addQuadCurve(
+                    to: CGPoint(x: calculate(side: width, toPoint: 162), y: calculate(side: height, toPoint: 35)),
+                    control: CGPoint(x: calculate(side: width, toPoint: 117), y: .zero)
+                )
+                
+                // puff (red)
+                path.addQuadCurve(
+                    to: CGPoint(x: calculate(side: width, toPoint: 207), y: calculate(side: height, toPoint: 66)),
+                    control: CGPoint(x: calculate(side: width, toPoint: 193), y: calculate(side: height, toPoint: 43))
+                )
+                
+                // puff (blue)
+                path.addQuadCurve(
+                    to: CGPoint(x: calculate(side: width, toPoint: 190), y: calculate(side: height, toPoint: 125)),
+                    control: CGPoint(x: calculate(side: width, toPoint: 233), y: calculate(side: height, toPoint: 101))
+                )
+            }
         }
-        .padding()
+    }
+    
+    private func calculate(side: Double, toPoint point: Double) -> Double {
+        let intermediateResult = side / point
+        return side / intermediateResult
     }
 }
 
 #Preview {
     ContentView()
+        .frame(width: 250, height: 150)
 }
