c1356fe6b9179d07e5328a7eac81f364afcb0ac3
Pave1iOS <PGribachev94@yandex.ru>
2024-03-18 14:34:57 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Pavel Gribachev on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
