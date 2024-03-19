0b91c0e1e2df0363e2ba8b28e291a870734b3c40
Pave1iOS <PGribachev94@yandex.ru>
2024-03-19 22:21:32 +0300

    add animation

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @State private var start = false
     
+    var foreverAnimation: Animation {
+        Animation.easeInOut(duration: 2.0)
+            .repeatForever(autoreverses: true)
+    }
+    
     var body: some View {
         ZStack {
             Color.white
@@ -17,19 +22,59 @@ struct ContentView: View {
             VStack {
                 ZStack {
                     SunView()
+                        .animation(
+                            .easeInOut(duration: 2.0).repeatForever(),
+                            value: start
+                        )
+                        .offset(y: start
+                                ? UIScreen.main.bounds.width - 410
+                                : UIScreen.main.bounds.width - 440)
+                    
                     CloudView()
+                        .animation(
+                            .easeInOut(duration: 2.0).repeatForever(),
+                            value: start
+                        )
+                        .offset(x: start
+                                ? UIScreen.main.bounds.width - 390
+                                : UIScreen.main.bounds.width - 410)
                 }
                 .frame(width: 300, height: 200)
             
                 HStack(spacing: 50) {
                     DropView().frame(width: 50, height: 50)
+                        .animation(
+                            .easeInOut(duration: 1.5).repeatForever(),
+                            value: start
+                        )
+                        .offset(y: start
+                                ? UIScreen.main.bounds.width - 320
+                                : UIScreen.main.bounds.width - 410)
+                    
                     DropView().frame(width: 50, height: 50)
+                        .animation(
+                            .easeInOut(duration: 1.8).repeatForever(),
+                            value: start
+                        )
+                        .offset(y: start
+                                ? UIScreen.main.bounds.width - 320
+                                : UIScreen.main.bounds.width - 410)
                 }
                 DropView().frame(width: 50, height: 50)
+                    .animation(
+                        .easeInOut(duration: 2.0).repeatForever(),
+                        value: start
+                    )
+                    .offset(y: start
+                            ? UIScreen.main.bounds.width - 320
+                            : UIScreen.main.bounds.width - 470)
             }
             
         }
         .frame(width: 350, height: 700)
+        .onAppear() {
+            start.toggle()
+        }
     }
 }
 
